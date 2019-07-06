59eead40b05d6667ca52126e8658ed23b3999d80
”Nanayaku” <sabakan789@gmail.com>
2019-07-06 12:45:56 +0000

    login-function

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get 'homes/sitemap'
   
   resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
